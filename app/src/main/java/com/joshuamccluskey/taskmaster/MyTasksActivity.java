6ccbcc2507c2b7341e42102ce565eec492f0dd8e
joshuamccluskey <jpiff57@gmail.com>
2022-03-22 15:46:54 -0700

    Username on My Tasks and username save in settings compelete

--- app/src/main/java/com/joshuamccluskey/taskmaster/MyTasksActivity.java
+++ app/src/main/java/com/joshuamccluskey/taskmaster/MyTasksActivity.java
@@ -1,55 +1,82 @@
 package com.joshuamccluskey.taskmaster;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MyTasksActivity extends AppCompatActivity {
 
     public final String TAG = "MainActivity";
+    SharedPreferences userPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_tasks);
+        userPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
-        Button addTaskButton = findViewById(R.id.goToAddTaskButton);
-        Button addAllTasksButton = findViewById(R.id.allTasksButton);
-        ImageButton addSettingsImageButton = findViewById(R.id.settingsImageButton);
+        addTaskButtonSetUp();
+        addAllTasksButtonSetUp();
+        addSettingsImageButtonSetUp();
 
-        addTaskButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                System.out.println("Add Task Button!");
-                Log.e(TAG, "onClick: Add Task Button!");
-                Intent goToAddTaskIntent = new Intent(MyTasksActivity.this, AddTaskActivity.class);
-                MyTasksActivity.this.startActivity(goToAddTaskIntent);
-            }
-        });
 
-        addAllTasksButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                System.out.println("All Tasks Button!");
-                Log.e(TAG, "onClick: All Tasks Button!");
-                Intent goToAllTasksIntent = new Intent(MyTasksActivity.this, AllTasksActivity.class);
-                startActivity(goToAllTasksIntent);
-            }
-        });
-
-        addSettingsImageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                System.out.println("Settings Image Button!");
-                Log.e(TAG, "onClick: Settings Image Button!");
-                Intent goToSettingsIntent = new Intent(MyTasksActivity.this, SettingsActivity.class);
-                startActivity(goToSettingsIntent);
-            }
-        });
     }
-}
+
+
+
+        @Override
+        public void onResume(){
+            super.onResume();
+
+            String username = userPreferences.getString(SettingsActivity.USERNAME_TAG, "No Username");
+            ((TextView)findViewById(R.id.usernameTextView)).setText(getString(R.string.username_username, username));
+        }
+
+        public void addTaskButtonSetUp(){
+            Button addTaskButton = findViewById(R.id.goToAddTaskButton);
+            addTaskButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    System.out.println("Add Task Button!");
+                    Log.e(TAG, "onClick: Add Task Button!");
+                    Intent goToAddTaskIntent = new Intent(MyTasksActivity.this, AddTaskActivity.class);
+                    MyTasksActivity.this.startActivity(goToAddTaskIntent);
+                }
+            });
+        }
+
+        public void addAllTasksButtonSetUp() {
+
+            Button addAllTasksButton = findViewById(R.id.allTasksButton);
+            addAllTasksButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    System.out.println("All Tasks Button!");
+                    Log.e(TAG, "onClick: All Tasks Button!");
+                    Intent goToAllTasksIntent = new Intent(MyTasksActivity.this, AllTasksActivity.class);
+                    startActivity(goToAllTasksIntent);
+                }
+            });
+        }
+
+        public void addSettingsImageButtonSetUp() {
+            ImageButton addSettingsImageButton = findViewById(R.id.settingsImageButton);
+            addSettingsImageButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    System.out.println("Settings Image Button!");
+                    Log.e(TAG, "onClick: Settings Image Button!");
+                    Intent goToSettingsIntent = new Intent(MyTasksActivity.this, SettingsActivity.class);
+                    startActivity(goToSettingsIntent);
+                }
+            });
+        }
+    }

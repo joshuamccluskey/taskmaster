21d0fc9dbbd8ec012f5116a27729961c88d7e40d
joshuamccluskey <jpiff57@gmail.com>
2022-03-23 14:31:17 -0700

    Task Class Model Complete

--- app/src/main/java/com/joshuamccluskey/taskmaster/model/Task.java
+++ app/src/main/java/com/joshuamccluskey/taskmaster/model/Task.java
@@ -5,4 +5,34 @@ public class Task {
     String body;
     State state;
 
+    public Task(String title, String body, State state) {
+        this.title = title;
+        this.body = body;
+        this.state = state;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
 }

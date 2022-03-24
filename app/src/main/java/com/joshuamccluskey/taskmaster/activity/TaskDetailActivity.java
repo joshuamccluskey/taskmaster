216703336f8b679d6cba49bcf5e681fa9a0ad6f8
joshuamccluskey <jpiff57@gmail.com>
2022-03-24 16:29:09 -0700

    Fixed Bug; Updated README with more screenshots; Built new APK

--- app/src/main/java/com/joshuamccluskey/taskmaster/activity/TaskDetailActivity.java
+++ app/src/main/java/com/joshuamccluskey/taskmaster/activity/TaskDetailActivity.java
@@ -2,6 +2,7 @@ package com.joshuamccluskey.taskmaster.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -22,8 +23,8 @@ public class TaskDetailActivity extends AppCompatActivity {
     @Override
     public void onResume(){
         super.onResume();
-
-        String taskTitle = userPreferences.getString(MyTasksActivity.TASK_DETAIL_TITLE_TAG, "No Task");
+        Intent callingIntent = getIntent();
+        String taskTitle = callingIntent.getStringExtra(MyTasksActivity.TASK_DETAIL_TITLE_TAG);
         ((TextView)findViewById(R.id.titleTaskDetailTextView)).setText(getString(R.string.task_title, taskTitle));
     }
 

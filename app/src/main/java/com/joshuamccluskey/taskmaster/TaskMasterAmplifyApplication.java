54e3c59e5a8c5661fe26966d9a8d844c097634b6
joshuamccluskey <jpiff57@gmail.com>
2022-03-29 17:51:21 -0700

    GraphQL Model Refactored for Task

---
+++ app/src/main/java/com/joshuamccluskey/taskmaster/TaskMasterAmplifyApplication.java
@@ -0,0 +1,13 @@
+package com.joshuamccluskey.taskmaster;
+
+import android.app.Application;
+
+import com.amplifyframework.core.Amplify;
+
+public class TaskMasterAmplifyApplication extends Application {
+
+    @Override
+    public void onCreate(){
+        
+    }
+}

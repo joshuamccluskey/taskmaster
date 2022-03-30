78113179267a4de2c73aff285eea4cb5a493dfbd
joshuamccluskey <jpiff57@gmail.com>
2022-03-29 19:54:38 -0700

    Taskmaster amplify application created

--- app/src/main/java/com/joshuamccluskey/taskmaster/TaskMasterAmplifyApplication.java
+++ app/src/main/java/com/joshuamccluskey/taskmaster/TaskMasterAmplifyApplication.java
@@ -1,13 +1,22 @@
 package com.joshuamccluskey.taskmaster;
 
 import android.app.Application;
+import android.util.Log;
 
+import com.amplifyframework.AmplifyException;
 import com.amplifyframework.core.Amplify;
 
 public class TaskMasterAmplifyApplication extends Application {
-
+    public static final String TAG = "TASKMASTER APPLICATION"
     @Override
     public void onCreate(){
+        super.onCreate();
+        try{
+            Amplify.configure(getApplicationContext());
+        } catch (AmplifyException ae) {
+            Log.e(TAG, "onCreate: " + ae.getMessage(), ae);
+            
+        }
         
     }
 }

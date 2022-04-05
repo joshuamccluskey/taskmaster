6defd9d0ae7923f0f0dbb4ac9ad3b215c9b7cc8b
joshuamccluskey <jpiff57@gmail.com>
2022-04-05 08:44:07 -0700

    Login and signup buttons wired up

--- app/src/main/java/com/joshuamccluskey/taskmaster/activity/LoginActivity.java
+++ app/src/main/java/com/joshuamccluskey/taskmaster/activity/LoginActivity.java
@@ -2,15 +2,44 @@ package com.joshuamccluskey.taskmaster.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 import com.joshuamccluskey.taskmaster.R;
 
 public class LoginActivity extends AppCompatActivity {
-
+    public final String TAG = "LogInActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginLoginButtonSetUp();
+        loginSignUpButtonSetUp();
+    }
+
+    public void loginLoginButtonSetUp(){
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(view -> {
+            System.out.println("Login Button!");
+            Log.e(TAG, "onClick: Login Button!");
+
+            //TODO Add Login
+            Intent goToLoginIntent = new Intent(LoginActivity.this, VerifyAccountActivity.class);
+            LoginActivity.this.startActivity(goToLoginIntent);
+        });
+    }
+
+
+    public void loginSignUpButtonSetUp(){
+        Button loginSignUpButton = findViewById(R.id.loginSignUpButton);
+        loginSignUpButton.setOnClickListener(view -> {
+            System.out.println("Signup Button!");
+            Log.e(TAG, "onClick: Signup Button!");
+            Intent goToSignUpIntent = new Intent(LoginActivity.this, SignupActivity.class);
+            LoginActivity.this.startActivity(goToSignUpIntent);
+        });
     }
 }
